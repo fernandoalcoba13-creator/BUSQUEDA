4cb922633bdb86fd531d32a8d6290a0485356f58
fernandoalcoba13-creator <fernandoalcoba13@gmail.com>
2026-03-15 19:51:17 -0300

    Update cults3d.py

--- cults3d.py
+++ cults3d.py
@@ -1,7 +1,2 @@
-from utils.normalize import encode_q
-
-
-async def search(query: str) -> list[dict]:
-    # TODO: reemplazar por scraper/API real
-    _ = encode_q(query)
+def search(query):
     return []
